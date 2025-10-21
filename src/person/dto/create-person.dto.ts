2550b5c125e0129e27d01a044d3bb89ec4d50123
Paulo Eduardo <pauloedu.1510@gmail.com>
2025-10-20 22:30:06 -0300

    primeiro

--- src/person/dto/create-person.dto.ts
+++ src/person/dto/create-person.dto.ts
@@ -1,1 +1,18 @@
-export class CreatePersonDto {}
+import { IsCPF, IsDate } from "@clockinit/brazilian-class-validator";
+import {  IsEmail, IsString, Length } from "class-validator";
+
+export class CreatePersonDto {
+    @IsString({ message: 'Título deve ser uma string' })
+    @Length(1, 100, { message: 'Título deve ter entre 1 e 100 caracteres' })
+    name: string;
+
+    @IsEmail({}, { message: 'Email inválido' })
+    email: string;
+
+    @IsDate({ message: 'Data de nascimento inválida' })
+    nascimento: Date;
+    
+    @IsCPF({ message: 'CPF inválido' })
+    cpf: string;
+}
+
--- src/person/dto/update-person.dto.ts
+++ src/person/dto/update-person.dto.ts
@@ -2,3 +2,4 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreatePersonDto } from './create-person.dto';
 
 export class UpdatePersonDto extends PartialType(CreatePersonDto) {}
+
